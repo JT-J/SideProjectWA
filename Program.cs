85ce2363b8ec6a8f4825825dc64e51e70672fdc2
JT-J <Jonathan.Johansson@ymail.com>
2024-07-02 23:55:40 +0200

    added db and server logic

--- Program.cs
+++ Program.cs
@@ -1,10 +1,16 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.EntityFrameworkCore;
 using SideProjectWA;
+using SideProjectWA.Services;
+using System.ComponentModel;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddDbContext<UserContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<IUserService, IUserService>();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
