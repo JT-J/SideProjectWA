f9024229b2306afa2f321f16a3c2f07fce7290e7
JT-J <Jonathan.Johansson@ymail.com>
2024-07-31 00:26:49 +0200

    added login functionality and login landing page

--- Program.cs
+++ Program.cs
@@ -3,14 +3,15 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.EntityFrameworkCore;
 using SideProjectWA;
 using SideProjectWA.Services;
+using System.Net.Http;
 using System.ComponentModel;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-builder.Services.AddDbContext<UserContext>(options =>
+builder.Services.AddDbContextFactory<UserContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-builder.Services.AddScoped<IUserService, IUserService>();
+builder.Services.AddScoped<IUserService, UserService>();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
